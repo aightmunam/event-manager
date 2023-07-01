ec0ac8dc9f9208976dfeb5d2d5033cba696b5b95
Munam Mubashir <10995414-aightmunam@users.noreply.gitlab.com>
2023-07-01 14:39:51 +0500

    Add ID field, refactor code for flake8, fix uncaught exception issue

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from chalice import Chalice, BadRequestError, NotFoundError, Response
-from schemas import UserSchema, EventSchema, EventRegistrationSchema
+from chalice import BadRequestError, Chalice, NotFoundError, Response
 from marshmallow import ValidationError
-from models import User, Event, Registration
+
+from models import Event, Registration, User
+from schemas import EventRegistrationSchema, EventSchema, UserSchema
+
+app = Chalice(app_name="events-manager")
 
 
-app = Chalice(app_name='events-manager')
-
-
-@app.route('/users', methods=['POST'])
+@app.route("/users", methods=["POST"])
 def create_user():
     request = app.current_request
     try:
@@ -15,55 +15,29 @@ def create_user():
         return Response(
             body=UserSchema().dump(user),
             status_code=200,
-            headers={'Content-Type': 'text/json'}
+            headers={"Content-Type": "text/json"}
         )
     except ValidationError as err:
         raise BadRequestError(err.messages_dict)
 
 
-@app.route('/users/{pk}', methods=['GET'])
-def get_user(pk):
-    full_key: str = User.prepare_key(pk)
-    try:
-        user: User = User.get(full_key, full_key)
-        return Response(
-            body=UserSchema().dump(user),
-            status_code=200,
-            headers={'Content-Type': 'text/json'}
-        )
-    except User.DoesNotExist:
-        raise NotFoundError("User not found")
-
-
-@app.route('/users/{pk}', methods=['PUT'])
-def update_user(pk):
+@app.route("/users/{pk}", methods=["GET", "DELETE", "PUT"])
+def user_detail(pk):
     request = app.current_request
     full_key: str = User.prepare_key(pk)
     try:
         user: User = User.get(full_key, full_key)
-        updated_user: User = UserSchema(context={"instance": user}).load(request.json_body)
-        return Response(
-            body=UserSchema().dump(updated_user),
-            status_code=200,
-            headers={'Content-Type': 'text/json'}
-        )
-    except User.DoesNotExist:
-        raise NotFoundError("User not found")
+        if request.method == "DELETE":
+            User.delete_user(user)
+        elif request.method == "PUT":
+            user = UserSchema(
+                context={"instance": user}
+            ).load(request.json_body)
 
-    except ValidationError as err:
-        raise BadRequestError(err.messages_dict)
-
-
-@app.route('/users/{pk}', methods=['DELETE'])
-def delete_user(pk):
-    full_key: str = User.prepare_key(pk)
-    try:
-        user: User = User.get(full_key, full_key)
-        User.delete_user(user)
         return Response(
             body=UserSchema().dump(user),
             status_code=200,
-            headers={'Content-Type': 'text/json'}
+            headers={"Content-Type": "text/json"}
         )
     except User.DoesNotExist:
         raise NotFoundError("User not found")
@@ -72,7 +46,7 @@ def delete_user(pk):
         raise BadRequestError(err.messages_dict)
 
 
-@app.route('/events', methods=['POST'])
+@app.route("/events", methods=["POST"])
 def create_event():
     request = app.current_request
     try:
@@ -80,34 +54,29 @@ def create_event():
         return Response(
             body=EventSchema().dump(event),
             status_code=200,
-            headers={'Content-Type': 'text/json'}
+            headers={"Content-Type": "text/json"}
         )
     except ValidationError as err:
         raise BadRequestError(err.messages_dict)
 
 
-@app.route('/users/{id}/events', methods=['GET'])
-def get_events_by_user(id):
-    full_key = User.prepare_key(id)
-    events = Event.gsi1.query(full_key, Event.gsi1SK.startswith("EVENT#"), filter_condition=Event.entity_type == "Event")
-    return Response(
-        body=EventSchema().dump(events, many=True),
-        status_code=200,
-        headers={'Content-Type': 'text/json'}
-    )
-
-
-@app.route('/events/{id}/registrations', methods=['POST'])
-def register_for_event(id):
+@app.route("/events/{pk}", methods=["GET", "DELETE", "PUT"])
+def event_detail(pk):
     request = app.current_request
-    full_key: str = Event.prepare_key(id)
+    full_key: str = Event.prepare_key(pk)
     try:
         event: Event = Event.get(full_key, full_key)
-        registration: Registration = EventRegistrationSchema().load({**request.json_body, "event": event.PK})
+        if request.method == "DELETE":
+            event.delete()
+        elif request.method == "PUT":
+            event = EventSchema(
+                context={"instance": event}
+            ).load(request.json_body)
+
         return Response(
-            body=EventRegistrationSchema().dump(registration),
+            body=EventSchema().dump(event),
             status_code=200,
-            headers={'Content-Type': 'text/json'}
+            headers={"Content-Type": "text/json"}
         )
     except Event.DoesNotExist:
         raise NotFoundError("Event not found")
@@ -116,17 +85,51 @@ def register_for_event(id):
         raise BadRequestError(err.messages_dict)
 
 
-@app.route('/events/{id}/registrations/{user_id}', methods=['GET', 'DELETE'])
+@app.route("/users/{id}/events", methods=["GET"])
+def get_events_by_user(id):
+    full_key = User.prepare_key(id)
+    events = Event.gsi1.query(full_key, Event.gsi1SK.startswith("EVENT#"))
+    return Response(
+        body=EventSchema().dump(events, many=True),
+        status_code=200,
+        headers={"Content-Type": "text/json"}
+    )
+
+
+@app.route("/events/{id}/registrations", methods=["POST"])
+def register_for_event(id):
+    request = app.current_request
+    full_key: str = Event.prepare_key(id)
+    try:
+        event: Event = Event.get(full_key, full_key)
+        registration: Registration = EventRegistrationSchema().load(
+            {**request.json_body, "event": event.ID}
+        )
+        return Response(
+            body=EventRegistrationSchema().dump(registration),
+            status_code=200,
+            headers={"Content-Type": "text/json"}
+        )
+    except Event.DoesNotExist:
+        raise NotFoundError("Event not found")
+
+    except ValidationError as err:
+        raise BadRequestError(err.messages_dict)
+
+
+@app.route("/events/{id}/registrations/{user_id}", methods=["GET", "DELETE"])
 def change_registration(id, user_id):
     request = app.current_request
     try:
-        registration: Registration = Registration.get(Event.prepare_key(id), User.prepare_key(user_id))
-        if request.method == 'DELETE':
+        registration: Registration = Registration.get(
+            Event.prepare_key(id), User.prepare_key(user_id)
+        )
+        if request.method == "DELETE":
             registration.delete()
         return Response(
             body=EventRegistrationSchema().dump(registration),
             status_code=200,
-            headers={'Content-Type': 'text/json'}
+            headers={"Content-Type": "text/json"}
         )
 
     except Registration.DoesNotExist:
@@ -136,11 +139,45 @@ def change_registration(id, user_id):
         raise BadRequestError(err.messages_dict)
 
 
-@app.route('/users/{id}/registrations', methods=['GET'])
-def get_user_registrations(id):
-    registrations = Registration.gsi1.query(User.prepare_key(id), filter_condition=Registration.entity_type == "Registration")
+@app.route("/events/{id}/registrations", methods=["GET"])
+def get_event_registrations(id):
+    registrations = Registration.query(
+        Event.prepare_key(id), Registration.SK.startswith("USER#")
+    )
     return Response(
         body=EventRegistrationSchema().dump(registrations, many=True),
         status_code=200,
-        headers={'Content-Type': 'text/json'}
+        headers={"Content-Type": "text/json"}
+    )
+
+
+@app.route("/users/{id}/registrations", methods=["GET"])
+def get_user_registrations(id):
+    registrations = Registration.gsi1.query(
+        User.prepare_key(id), Registration.gsi1SK.startswith("REGISTRATION#")
+    )
+    return Response(
+        body=EventRegistrationSchema().dump(registrations, many=True),
+        status_code=200,
+        headers={"Content-Type": "text/json"}
+    )
+
+
+@app.route("/events", methods=["GET"])
+def get_events_for_city():
+    query_params = app.current_request.query_params
+
+    if not query_params or "city" not in query_params:
+        raise BadRequestError({"city": "`city` query param must be provided"})
+
+    city: str = query_params.get("city")
+    zip_code: str = query_params.get("zip_code")
+    if zip_code:
+        events = Event.gsi2.query(city, Event.gsi2SK == zip_code)
+    else:
+        events = Event.gsi2.query(city)
+    return Response(
+        body=EventSchema().dump(events, many=True),
+        status_code=200,
+        headers={"Content-Type": "text/json"}
     )
--- schemas.py
+++ schemas.py
@@ -1,14 +1,14 @@
-from marshmallow import Schema, fields, post_load, ValidationError, post_dump
-from ulid import ULID
-from models import User, Event, Registration
-
-from pynamodb.exceptions import PutError
 from datetime import datetime, timezone
+
+from marshmallow import Schema, ValidationError, fields, post_load
+from pynamodb.exceptions import PutError
+from ulid import ULID
+
+from models import Event, Registration, User
 
 
 class BaseSchema(Schema):
-    PK = fields.Str()
-    SK = fields.Str()
+    ID = fields.Str(dump_only=True)
 
 
 class UserSchema(BaseSchema):
@@ -21,14 +21,17 @@ class UserSchema(BaseSchema):
     @post_load
     def make_user(self, data, **kwargs) -> User:
         instance: User | None = self.context.get("instance", None)
-        prepared_key: str = User.prepare_key(str(ULID()))
-        PK = SK = None
         if instance:
             PK: str = instance.PK
             SK: str = instance.SK
+        else:
+            identifier: str = str(ULID())
+            data["ID"] = identifier
+            prepared_key: str = User.prepare_key(identifier)
+            PK: str = prepared_key
+            SK: str = prepared_key
 
-        put_data: dict = {"PK": PK or prepared_key, "SK": SK or prepared_key, **data}
-        user: User = User(**put_data)
+        user: User = User(PK, SK, **data)
         User.put_user(user, old_user=instance)
         return user
 
@@ -40,7 +43,7 @@ class EventSchema(BaseSchema):
     description = fields.Str(required=True)
     date = fields.AwareDateTime(required=True)
     city = fields.Str(required=True)
-    zip_code = fields.Number(required=True)
+    zip_code = fields.Str(required=True)
 
     created_by = fields.Str(required=True)
 
@@ -52,17 +55,21 @@ class EventSchema(BaseSchema):
 
     @post_load
     def make_event(self, data, **kwargs) -> Event:
-        prepared_key: str = Event.prepare_key(str(ULID()))
-        data["created_by"] = User.prepare_key(data["created_by"])
-        PK: str = data.get("PK") or prepared_key
-        SK: str = data.get("SK") or prepared_key
+        if instance := self.context.get("instance", None):
+            identifier: str = instance.ID
+            event_id: str = instance.PK
+        else:
+            identifier: str = str(ULID())
+            event_id: str = Event.prepare_key(identifier)
+            data["ID"] = identifier
+
         index_data: dict = {
-            "gsi1PK": data["created_by"],
-            "gsi1SK": PK,
+            "gsi1PK": User.prepare_key(data["created_by"]),
+            "gsi1SK": event_id,
             "gsi2PK": data["city"],
             "gsi2SK": data["zip_code"]
         }
-        event: Event = Event(PK, SK, **data, **index_data)
+        event: Event = Event(event_id, event_id, **data, **index_data)
         event.save()
         return event
 
@@ -79,17 +86,27 @@ class EventRegistrationSchema(BaseSchema):
         if not data.get("registration_time"):
             data["registration_time"] = datetime.now(timezone.utc)
 
-        data["user"] = User.prepare_key(data["user"])
-
-        PK: str = data.get("PK") or data["event"]  # event_id is the partition key
-        SK: str = data.get("SK") or data["user"]  # user_id is the sort key
+        data["ID"] = str(ULID())
         index_data: dict = {
-            "gsi1PK": SK,
-            "gsi1SK": PK,
+            "gsi1PK": User.prepare_key(data["user"]),
+            "gsi1SK": Registration.prepare_key(data["event"]),
         }
         try:
-            registration: Registration = Registration(PK, SK, **data, **index_data)
-            registration.save(Registration.PK.does_not_exist() & Registration.SK.does_not_exist())
-        except PutError as _exc:
-            raise ValidationError({"user": "Registration failed! User may already be registered for this event"})
+            registration: Registration = Registration(
+                Event.prepare_key(data["event"]),
+                User.prepare_key(data["user"]),
+                **data,
+                **index_data
+            )
+            registration.save(
+                Registration.PK.does_not_exist()
+                & Registration.SK.does_not_exist()
+            )
+        except PutError:
+            raise ValidationError(
+                {
+                    "user": "Registration failed! User may already be"
+                    " registered for this event"
+                }
+            )
         return registration
